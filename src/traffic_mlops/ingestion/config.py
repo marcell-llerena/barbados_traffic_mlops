77e1d6f089cfdc6131ccbda7c3489ac97b56b666
marcell-llerena <marcell.llerenaai@gmail.com>
2026-02-14 12:52:33 -0500

    feat: add YOLO vehicle tracker configuration model

---
+++ src/traffic_mlops/ingestion/config.py
@@ -0,0 +1,38 @@
+from pathlib import Path
+
+from pydantic import BaseModel
+from pydantic import Field
+
+
+class YoloVehicleTrackerConfig(BaseModel):
+    model_name: str = Field(
+        ...,
+        description="Name of the YOLO model to use for vehicle tracking.",
+    )
+    input_dir: Path = Field(
+        ...,
+        description="Directory containing the input video files.",
+    )
+    output_dir: Path = Field(
+        ...,
+        description="Directory to save the output video files.",
+    )
+    n_workers: int = Field(
+        default=4,
+        description="Number of workers to use for processing the video files.",
+    )
+    confidence: float = Field(
+        default=0.5,
+        description="Confidence threshold for the YOLO model.",
+    )
+    device: str | None = Field(
+        default=None,
+        description="Device to use for processing.",
+    )
+    vehicle_classes: list[int] = Field(
+        default=[2, 3, 5, 7],
+        description=(
+            "Classes of vehicles to track. The classes are defined in the YOLO model."
+            "See https://docs.ultralytics.com/models/detect/#classes for more details."
+        ),
+    )
